d03ee3ee168865ee6ca6f447730a65d8e66cd3f1
deusestmortuus <kuznetsov1398@gmail.com>
2021-03-04 02:29:12 +0700

    task 3

--- src/main/java/ftc/shift/sample/repository/LicenceRepository.java
+++ src/main/java/ftc/shift/sample/repository/LicenceRepository.java
@@ -4,8 +4,10 @@ import ftc.shift.sample.entity.Licence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface LicenceRepository extends JpaRepository<Licence, UUID> {
+    List<Licence> findLicencesByUserId(Long companyId);
 }
