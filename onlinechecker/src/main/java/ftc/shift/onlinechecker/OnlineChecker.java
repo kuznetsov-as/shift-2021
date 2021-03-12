1ed104449209e75e1aa7118222d3969c16face83
Сидоров Александр Викоторович <a.sidorov1@g.nsu.ru>
2021-03-12 14:33:14 +0700

    Adapted for versioning

--- onlinechecker/src/main/java/ftc/shift/onlinechecker/OnlineChecker.java
+++ onlinechecker/src/main/java/ftc/shift/onlinechecker/OnlineChecker.java
@@ -1,6 +1,7 @@
 package ftc.shift.onlinechecker;
 
 import ftc.shift.onlinechecker.exception.LicenceException;
+import lombok.NonNull;
 
 import java.io.IOException;
 import java.net.URI;
@@ -9,10 +10,17 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class OnlineChecker {
-    public static boolean isLicenceAvailable(String url, String licence) throws IOException, InterruptedException, LicenceException {
+    public static boolean isLicenceAvailable(@NonNull String url,
+                                             @NonNull String licence,
+                                             @NonNull String productType,
+                                             @NonNull String productVersion)
+            throws IOException, InterruptedException, LicenceException {
+
         HttpClient httpClient = HttpClient.newHttpClient();
-        // TODO: 12.03.2021 адаптировать под версионность, жду последню версию 
-        HttpRequest httpRequest = HttpRequest.newBuilder(URI.create(url + "/licences/check"))
+
+        var uri = String.format(url + "/licences/check?productType=%s&productVersion=%s", productType, productVersion);
+
+        HttpRequest httpRequest = HttpRequest.newBuilder(URI.create(uri))
                 .header("Content-Type", "text/plain")
                 .POST(HttpRequest.BodyPublishers.ofString(licence))
                 .build();
