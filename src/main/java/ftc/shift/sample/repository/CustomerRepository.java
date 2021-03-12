512b2d49f8abe514c3802c61f189d5b6c97c1177
Ekaterina Shevchuk <e.shevchuk@g.nsu.ru>
2021-03-12 11:13:50 +0700

    Fix conflict

--- src/main/java/ftc/shift/sample/repository/CustomerRepository.java
+++ src/main/java/ftc/shift/sample/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package ftc.shift.sample.repository;
 
 import ftc.shift.sample.entity.Customer;
+import ftc.shift.sample.entity.Licence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,5 +11,5 @@ import org.springframework.stereotype.Repository;
  */
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Licence> {
 }
